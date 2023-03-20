aae78bd2109a118b117aba780f9efe42ac9fd8b2
imygnam <imygnam@gmail.com>
2023-03-20 20:13:02 +0900

    [FIX] add log

--- src/main/kotlin/com/lolduo/restapiserver/service/ChampionServiceImpl.kt
+++ src/main/kotlin/com/lolduo/restapiserver/service/ChampionServiceImpl.kt
@@ -10,18 +10,22 @@ class ChampionServiceImpl (
     val championInfoRepository: ChampionInfoRepository,
 ) : ChampionService {
     override fun getChampionList(size:Int, locale: String): List<ChampionInfo> {
-
+        println(size)
+        println(locale)
         val champions = championInfoRepository.findByLocale(locale)
         val championEnUs = championInfoRepository.findByLocale("en_US")
-
+        println(champions)
+        println(championEnUs)
         val championInfoList = listOf<ChampionInfo>()
-        for(champion in champions) {
-            val championName = champion.name
-            val championEnUsName = championEnUs.find { it.id == champion.id }?.name ?: ""
+        for(champion in championEnUs) {
+            val championEnUsName = champion.name
+            println(championEnUsName)
+            val championName = champions.find { it.id == champion.id }?.name ?: ""
+            println(championName)
             val championNamList = listOf<ChampionName>()
-            championNamList.plus(ChampionName(locale, championName))
+            championNamList.plus(ChampionName("en_US", championEnUsName))
             if(championName != championEnUsName)
-                championName.plus(ChampionName("en_US", championEnUsName))
+                championName.plus(ChampionName(locale, championName))
             val imgUrl = "https://lolduo-static-img.s3.ap-northeast-2.amazonaws.com/champion/${size}/${champion.image.full}"
             championInfoList.plus(ChampionInfo(champion.id, championNamList, imgUrl))
         }

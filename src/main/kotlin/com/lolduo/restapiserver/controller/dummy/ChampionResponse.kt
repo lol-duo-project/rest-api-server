ee03d87ecb439fc556e45a44e21bb52a63db4de7
imygnam <imygnam@gmail.com>
2023-03-28 23:11:05 +0900

    [FEAT] delete private

--- src/main/kotlin/com/lolduo/restapiserver/controller/dummy/ChampionResponse.kt
+++ src/main/kotlin/com/lolduo/restapiserver/controller/dummy/ChampionResponse.kt
@@ -3,10 +3,10 @@ package com.lolduo.restapiserver.controller.dummy
 import io.swagger.annotations.ApiModelProperty
 
 class ChampionResponse(
-    private val championName: String,
-    @field:ApiModelProperty(example = "https://lol-duo-bucket.s3.ap-northeast-2.amazonaws.com/champion/Ziggs.svg") private val championImgUrl: String,
+     val championName: String,
+    @field:ApiModelProperty(example = "https://lol-duo-bucket.s3.ap-northeast-2.amazonaws.com/champion/Ziggs.svg")  val championImgUrl: String,
     @field:ApiModelProperty(
         example = "https://lol-duo-bucket.s3.ap-northeast-2.amazonaws.com/mainPage/mainRune/ArcaneComet.svg"
-    ) private val mainRuneImgUrl: String,
-    @field:ApiModelProperty(example = "https://lol-duo-bucket.s3.ap-northeast-2.amazonaws.com/mainPage/position/BOTTOM.svg") private val positionImgUrl: String
+    )  val mainRuneImgUrl: String,
+    @field:ApiModelProperty(example = "https://lol-duo-bucket.s3.ap-northeast-2.amazonaws.com/mainPage/position/BOTTOM.svg")  val positionImgUrl: String
 )
--- src/main/kotlin/com/lolduo/restapiserver/controller/dummy/DoubleResponseV2.kt
+++ src/main/kotlin/com/lolduo/restapiserver/controller/dummy/DoubleResponseV2.kt
@@ -3,26 +3,26 @@ package com.lolduo.restapiserver.controller.dummy
 import io.swagger.annotations.ApiModelProperty
 
 class DoubleResponseV2(
-    @field:ApiModelProperty(example = "15") private val id: Long,
+    @field:ApiModelProperty(example = "15") val id: Long,
     @field:ApiModelProperty(
         example = "0"
-    ) private val rankChangeNumber: Long,
-    @field:ApiModelProperty(example = "1") private val rankNumber: Long,
+    ) val rankChangeNumber: Long,
+    @field:ApiModelProperty(example = "1") val rankNumber: Long,
     @field:ApiModelProperty(
         example = "https://lol-duo-bucket.s3.ap-northeast-2.amazonaws.com/logo/Group.svg"
-    ) private val rankNumberIcon: String,
+    ) val rankNumberIcon: String,
     champion1: ChampionResponse,
     champion2: ChampionResponse,
     winRate: String
 ) {
     @ApiModelProperty
-    private val champion1: ChampionResponse
+     val champion1: ChampionResponse
 
     @ApiModelProperty
-    private val champion2: ChampionResponse
+     val champion2: ChampionResponse
 
     @ApiModelProperty(example = "67.2%")
-    private val winRate: String
+     val winRate: String
 
     init {
         this.champion1 = champion1
